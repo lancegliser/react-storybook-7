59fd9c5592a97a7d3f36babb67c7aeadcc6fc7e5
Lance Gliser <lance@fragmentedthought.com>
2023-03-30 19:58:29 -0500

    Added support for our first package Provider: Helmet (#2)
    * react-helmet-async@^1.3.0
    
    * Created an AppProviders component
    
    * Wrapped App in AppProviders using main.tsx
    
    * Updated the App to use the same title for the H1 and the HTML title element!
    
    * Updated README.md to note the react-helmet-async integration
    
    * Added our first Storybook global decorator to wrap all stories in <AppProviders />.

---
+++ .storybook/preview.tsx
@@ -0,0 +1,27 @@
+import type {Preview, StoryFn} from "@storybook/react";
+import AppProviders from "../src/AppProviders";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
+
+export const decorators = [
+  (Story: StoryFn) => (
+      // Wrap all our stories in the same global <AppProviders /> to ensure we support global packages!
+      <AppProviders>
+          <Story />
+      </AppProviders>
+  ),
+];
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import AppProviders from "./AppProviders";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppProviders>
+        <App />
+    </AppProviders>
   </React.StrictMode>,
 )
