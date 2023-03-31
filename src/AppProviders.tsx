d9c0813c0ecc4fd3cf256e2e1f371ed18c6335c9
Lance Gliser <lance.gliser@torch.ai>
2023-03-30 19:52:56 -0500

    Added our first Storybook global decorator to wrap all stories in <AppProviders />.

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -6,9 +6,16 @@ interface AppProvidersProps {
 }
 const AppProviders: React.FunctionComponent<AppProvidersProps> = ({children}) => {
     return (
-        <HelmetProvider>
-            {children}
-        </HelmetProvider>
+        <>
+            {/*
+              Errors indicating you might be missing HelmetProvider:
+              Dispatcher.js:60 Uncaught TypeError: Cannot read properties of undefined (reading 'add')
+              *react-dom.development.js:18687 The above error occurred in the <HelmetDispatcher> component:
+            */}
+            <HelmetProvider>
+                {children}
+            </HelmetProvider>
+        </>
     )
 }
 
